bb7d4ad3b57701dd38da5ec9aac3d4283bd8b201
Walczyk <walczyk.kamil@gmail.com>
2025-08-22 09:52:51 +0200

    update seo module, add favicons and logo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
 import "./src/styles/global.css"
+
+document.querySelectorAll("iframe").forEach((iframe) => {
+  if (!iframe.hasAttribute("title")) {
+    iframe.setAttribute("title", "Google Tag Manager");
+  }
+});
--- src/components/Seo.js
+++ src/components/Seo.js
@@ -3,40 +3,47 @@ import { Helmet } from 'react-helmet';
 
 const Seo = () => {
   const pathname = typeof window !== "undefined" ? window.location.pathname : "/";
-
-  const siteUrl = 'https://danutakoziello-doherty.co.uk/';
+  const siteUrl = 'https://danutakoziello-doherty.co.uk';
   const faviconPath = '/favicon.ico';
   const ogImagePath = '/og-image.webp';
 
-  const schemaData = {
+  const personSchema = {
     "@context": "https://schema.org",
     "@type": "Person",
     "name": "Danuta Koziełło-Doherty",
     "jobTitle": "Psycholog, Psychoterapeuta",
     "description": "Psycholog i psychoterapeuta specjalizujący się w terapii traumy, teorii przywiązania i podejściach somatycznych.",
     "url": siteUrl,
-    "image": `${siteUrl}/og-image.webp`,
+    "image": `${siteUrl}${ogImagePath}`,
     "address": {
       "@type": "PostalAddress",
       "addressLocality": "Leeds",
       "addressRegion": "West Yorkshire",
       "addressCountry": "UK"
     }
-  }
+  };
+
+  const organizationSchema = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "url": siteUrl,
+    "name": "Danuta Koziełło-Doherty",
+    "logo": `${siteUrl}/logo.png`
+  };
 
   return (
     <Helmet>
       <html lang="pl" />
-      <title>Danuta Koziełło-Doherty – Psychoterapia i wsparcie terapeutyczne</title>
+      <title>Psychoterapia – Danuta Koziełło-Doherty</title>
       <meta
         name="description"
         content="Danuta Koziełło-Doherty – psycholog i psychoterapeuta. Oferuję terapię traumy, pracę z relacjami i wsparcie online oraz w gabinecie w Leeds."
       />
       <meta name="author" content="Danuta Koziełło-Doherty" />
-      <meta name="keywords" content="Psycholog, Psychoterapia indywidualna, Terapia indewidualna, Metody, Terapia z cialem, Leeds, WestYorkshire, Terapia Online,Polski psychoterapeta leeds" />
+      <meta name="keywords" content="Psycholog, Psychoterapia indywidualna, Terapia indywidualna, Metody, Terapia z ciałem, Leeds, West Yorkshire, Terapia Online, Polski psychoterapeuta Leeds" />
       <meta name="robots" content="index, follow" />
       <meta name="language" content="pl-PL" />
-      <link rel="canonical" href={`${siteUrl}${pathname || ""}`} />
+      <link rel="canonical" href={`${siteUrl}${pathname}`} />
 
       {/* Open Graph */}
       <meta property="og:type" content="website" />
@@ -45,26 +52,19 @@ const Seo = () => {
         property="og:description"
         content="Psycholog kliniczny i certyfikowany psychoterapeuta specjalizujący się w pracy z traumą i podejściami somatycznymi. Terapia indywidualna online i stacjonarnie."
       />
-      <meta property="og:url" content={siteUrl} />
+      <meta property="og:url" content={`${siteUrl}${pathname}`} />
       <meta property="og:image" content={`${siteUrl}${ogImagePath}`} />
       <meta property="og:locale" content="pl_PL" />
 
-      {/* Twitter Card */}
-      {/* <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Danuta Koziełło-Doherty – Psychoterapia i wsparcie terapeutyczne" />
-      <meta
-        name="twitter:description"
-        content="Psycholog kliniczny i certyfikowany psychoterapeuta specjalizujący się w terapii traumy i podejściach somatycznych. Terapia indywidualna online i stacjonarnie."
-      />
-      <meta name="twitter:image" content={`${siteUrl}${ogImagePath}`} />
-      <meta name="twitter:creator" content="@TwojTwitterHandle" /> */}
-
       {/* Favicon */}
-      <link rel="icon" href={`${siteUrl}${faviconPath}`} type="image/x-icon" />
+      <link rel="icon" href={`${faviconPath}`} type="image/x-icon" />
 
       {/* JSON-LD Schema.org */}
       <script type="application/ld+json">
-        {JSON.stringify(schemaData)}
+        {JSON.stringify(personSchema)}
+      </script>
+      <script type="application/ld+json">
+        {JSON.stringify(organizationSchema)}
       </script>
     </Helmet>
   );
